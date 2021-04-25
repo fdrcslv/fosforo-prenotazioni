6b8483492572727ece0cf0774d9b24e0e4af799d
Federico Silvestri <federico.silvestri@moveax.it>
2021-04-25 13:10:29 +0200

    add router and store

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store/store'
+
+
+
+
 
 require('@/assets/main.scss');
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
