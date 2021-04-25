f00916c17f49d8c72c86f01c7f039bc2751604ba
Federico Silvestri <federico.silvestri@moveax.it>
2021-04-25 12:19:30 +0200

    fix sass-loader version

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button class="button is-primary"> ahahaha </button>
   </div>
 </template>
 
